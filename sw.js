8a5038a6c8108657fdd0445c033c2b712353df86
Vblep <ebuvick0@gmail.com>
2025-06-24 15:25:23 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,60 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('nd1-cache').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/icon-192.png',
-        '/icon-512.png'
-      ]);
+const CACHE_NAME = 'nd1portal-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/auth.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/sw.js',
+  '/timetable.html',
+  '/notices.html',
+  '/hod.html',
+  '/reps.html',
+  '/gallery.html',
+  '/forum.html',
+  '/polls.html',
+  '/leaderboard.html',
+  '/events.html',
+  '/users.html',
+  '/approve.html',
+  '/view-profile.html',
+  '/username.html',
+  '/style.css', // if you use one
+  'https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js',
+  'https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js',
+  'https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js'
+];
+
+// Install Service Worker and cache static assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+// Activate Service Worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Fetch cached assets
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) =>
+      response || fetch(event.request)
+    )
   );
 });
